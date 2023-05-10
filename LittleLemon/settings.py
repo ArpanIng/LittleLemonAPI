cd1577b3c19dc1f03f0658d28a6e984bbad379d0
arpan <ingnamarpan@gmail.com>
2023-05-10 17:17:23 +0545

    Update settings.py

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -1,4 +1,9 @@
+import environ
 from pathlib import Path
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&%lvfv_@zllldclfw3*0j4(a$i2k5wp#qo9%@0iz$s^4oxo8zv"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
